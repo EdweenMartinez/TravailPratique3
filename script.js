aae53c937524a38386931bfcd7ce8dd5fe13a10b
2013668@bdeb.qc.ca <2013668@bdeb.qc.ca>
2025-12-10 12:30:04 -0500

    ajout de effet sur le panier du header quand on click sur un article

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-/*---------------------- Pour afficher le message ajouter au panier dans la page articles ---------------------------*/
+/*---------------------- Pour afficher le message ajouter au panier dans la page articles  et changement de icon cart ---------------------------*/
 
 document.querySelectorAll(".bArticle").forEach(article => {
     article.addEventListener("click", () => {
         alert("Ajouter au panier");
+
+        const cart = document.getElementById("cartHeader");
+
+       cart.src = "./IMages/cart_plein.png";
+       cart.style.height = "90px";
+       cart.style.width = "90px";
+
+       sessionStorage.setItem("cartPlein" ,"true");
+
+
+       
+       
     });
 });
+
+window.addEventListener("DOMContentLoaded",() =>{
+
+    const cart = document.getElementById("cartHeader");
+
+    if(sessionStorage.getItem("cartPlein") == "true"){
+         cart.src = "./IMages/cart_plein.png";
+       cart.style.height = "90px";
+       cart.style.width = "90px";
+    }
+})
+
+
+
+
+
+
 
 
 
